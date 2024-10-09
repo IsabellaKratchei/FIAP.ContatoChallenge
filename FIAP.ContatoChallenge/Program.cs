a04f93963461cce293642dd0ae71daac95c00132
bellakratchei <isabellavk@hotmail.com.br>
2024-10-09 20:15:11 -0300

    New: Adicionado validaçao de criaçao de base Adicionado script que popula a tabela de DDDs Fix: Refatorado a classe Program.cs (separado as responsabilidades/métodos ao buildar o programa)

--- FIAP.ContatoChallenge/Program.cs
+++ FIAP.ContatoChallenge/Program.cs
@@ -7,41 +7,105 @@ namespace FIAP.ContatoTechChallenge
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
-
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
-            builder.Services.AddEntityFrameworkSqlServer()
-                    .AddDbContext<BDContext>(o=> o.UseSqlServer(@"Server=DESKTOP-O0E2FJ0\SQLSERVER2022;Database=FIAP;Trusted_Connection=True;TrustServerCertificate=true;"));
-            builder.Services.AddScoped<IContatoRepository, ContatoRepository>();
-            builder.Services.AddScoped<IRegiaoRepository, RegiaoRepository>();
+            ConfigureServices(builder);
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+            await MigrateDatabaseAsync(app);
+
+            ConfigureMiddleware(app);
+
+            app.Run();
+        }
+
+        private static void ConfigureServices(WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            builder.Services.AddEntityFrameworkSqlServer()
+                    .AddDbContext<BDContext>(o => o.UseSqlServer(@"Server=DESKTOP-O0E2FJ0\SQLSERVER2022;Database=FIAP;Trusted_Connection=True;TrustServerCertificate=true;"));
+            builder.Services.AddScoped<IContatoRepository, ContatoRepository>();
+            builder.Services.AddScoped<IRegiaoRepository, RegiaoRepository>();
+        }
+
+        private static async Task MigrateDatabaseAsync(WebApplication app)
+        {
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<BDContext>();
+                    await GaranteCriacaoDatabaseAsync(context);
+                    await PopulaDDDSeVazioAsync(context);
+                }
+                catch (Exception ex)
+                {
+                    // Usar monitoramento de logs aqui, invés de WriteLine
+                    Console.WriteLine($"Erro ao verificar ou criar o banco de dados: {ex.Message}");
+                }
+            }
+        }
+
+        private static async Task GaranteCriacaoDatabaseAsync(BDContext context)
+        {
+            if (!context.Database.CanConnect())
+            {
+                Console.WriteLine("Banco de dados não encontrado, criando...");
+                context.Database.Migrate(); // Cria o banco e aplica as migrations
+                Console.WriteLine("Migration(s) aplicada(s) com sucesso.");
+            }
+            else
+            {
+                Console.WriteLine("Banco de dados encontrado.");
+            }
+        }
+
+        private static async Task PopulaDDDSeVazioAsync(BDContext context)
+        {
+            var existemRegistros = await context.DDDs.AnyAsync();
+
+            if (!existemRegistros)
+            {
+                var scriptPath = Path.Combine(Directory.GetCurrentDirectory(), "Scripts", "ScriptDDD.sql");
+                if (File.Exists(scriptPath))
+                {
+                    Console.WriteLine("Executando script para popular tabela de DDDs...");
+
+                    var scriptSql = await File.ReadAllTextAsync(scriptPath);
+                    await context.Database.ExecuteSqlRawAsync(scriptSql);
+                    Console.WriteLine("Tabela de DDDs populada com sucesso.");
+                }
+                else
+                {
+                    Console.WriteLine($"O arquivo de script '{scriptPath}' não foi encontrado.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Tabela de DDDs já está populada.");
+            }
+        }
+
+        private static void ConfigureMiddleware(WebApplication app)
+        {
             if (!app.Environment.IsDevelopment())
             {
-                app.UseExceptionHandler(" / Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseExceptionHandler("/Home/Error");
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
             app.UseAuthorization();
 
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
-
-            app.Run();
         }
     }
 }
---
+++ FIAP.ContatoChallenge/Scripts/ScriptDDD.sql
@@ -0,0 +1,74 @@
+﻿USE [FIAP]
+GO
+
+INSERT INTO DDDs (DDD, Regiao) VALUES
+('11', 'Sudeste'), -- São Paulo (Capital)
+('12', 'Sudeste'), -- São Paulo (Interior)
+('13', 'Sudeste'), -- São Paulo (Litoral)
+('14', 'Sudeste'), -- São Paulo (Interior)
+('15', 'Sudeste'), -- São Paulo (Interior)
+('16', 'Sudeste'), -- São Paulo (Interior)
+('17', 'Sudeste'), -- São Paulo (Interior)
+('18', 'Sudeste'), -- São Paulo (Interior)
+('19', 'Sudeste'), -- São Paulo (Interior)
+('21', 'Sudeste'), -- Rio de Janeiro (Capital)
+('22', 'Sudeste'), -- Rio de Janeiro (Interior)
+('24', 'Sudeste'), -- Rio de Janeiro (Interior)
+('27', 'Sudeste'), -- Espírito Santo (Capital e Norte)
+('28', 'Sudeste'), -- Espírito Santo (Sul)
+('31', 'Sudeste'), -- Minas Gerais (Capital)
+('32', 'Sudeste'), -- Minas Gerais (Zona da Mata)
+('33', 'Sudeste'), -- Minas Gerais (Vale do Rio Doce)
+('34', 'Sudeste'), -- Minas Gerais (Triângulo Mineiro)
+('35', 'Sudeste'), -- Minas Gerais (Sul)
+('37', 'Sudeste'), -- Minas Gerais (Centro-Oeste)
+('38', 'Sudeste'), -- Minas Gerais (Norte)
+('41', 'Sul'),     -- Paraná (Curitiba e região)
+('42', 'Sul'),     -- Paraná (Interior)
+('43', 'Sul'),     -- Paraná (Interior)
+('44', 'Sul'),     -- Paraná (Interior)
+('45', 'Sul'),     -- Paraná (Interior)
+('46', 'Sul'),     -- Paraná (Interior)
+('47', 'Sul'),     -- Santa Catarina (Norte)
+('48', 'Sul'),     -- Santa Catarina (Sul)
+('49', 'Sul'),     -- Santa Catarina (Oeste)
+('51', 'Sul'),     -- Rio Grande do Sul (Capital e região metropolitana)
+('53', 'Sul'),     -- Rio Grande do Sul (Litoral)
+('54', 'Sul'),     -- Rio Grande do Sul (Serra Gaúcha)
+('55', 'Sul'),     -- Rio Grande do Sul (Interior)
+('61', 'Centro-Oeste'), -- Distrito Federal
+('62', 'Centro-Oeste'), -- Goiás (Capital e região)
+('63', 'Norte'),    -- Tocantins
+('64', 'Centro-Oeste'), -- Goiás (Interior)
+('65', 'Centro-Oeste'), -- Mato Grosso (Capital)
+('66', 'Centro-Oeste'), -- Mato Grosso (Interior)
+('67', 'Centro-Oeste'), -- Mato Grosso do Sul
+('68', 'Norte'),    -- Acre
+('69', 'Norte'),    -- Rondônia
+('71', 'Nordeste'), -- Bahia (Capital e região metropolitana)
+('73', 'Nordeste'), -- Bahia (Sul)
+('74', 'Nordeste'), -- Bahia (Norte)
+('75', 'Nordeste'), -- Bahia (Recôncavo)
+('77', 'Nordeste'), -- Bahia (Oeste)
+('79', 'Nordeste'), -- Sergipe
+('81', 'Nordeste'), -- Pernambuco (Capital e região metropolitana)
+('82', 'Nordeste'), -- Alagoas
+('83', 'Nordeste'), -- Paraíba
+('84', 'Nordeste'), -- Rio Grande do Norte
+('85', 'Nordeste'), -- Ceará (Capital e região metropolitana)
+('86', 'Nordeste'), -- Piauí (Sul e capital)
+('87', 'Nordeste'), -- Pernambuco (Interior)
+('88', 'Nordeste'), -- Ceará (Interior)
+('89', 'Nordeste'), -- Piauí (Interior)
+('91', 'Norte'),    -- Pará (Capital e região metropolitana)
+('92', 'Norte'),    -- Amazonas (Capital e região)
+('93', 'Norte'),    -- Pará (Interior)
+('94', 'Norte'),    -- Pará (Interior)
+('95', 'Norte'),    -- Roraima
+('96', 'Norte'),    -- Amapá
+('97', 'Norte'),    -- Amazonas (Interior)
+('98', 'Nordeste'), -- Maranhão (Capital e região metropolitana)
+('99', 'Nordeste'); -- Maranhão (Interior)
+GO
+
+
